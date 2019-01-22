96a71fe201031ade693cd1d19c0afc880407bad8
Jeff Dinkelman <mfankenstein@yahoo.com>
2019-01-22 14:35:17 -0800

    updated mongoose.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 //import libraries for setting up the application server
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 //import the router module
 const products = require('./routes/api/products');
 
@@ -23,23 +24,17 @@ app.use(allowCrossDomain);
 
 app.use('/api/products', products);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
 
-
-
-
-
-//Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//   //Set static folder
-//   app.use(express.static('client/build'));
-//
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html '));
-//   });
-// };
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html '));
+  });
+};
 
 const port = process.env.PORT || 5000;
-
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
